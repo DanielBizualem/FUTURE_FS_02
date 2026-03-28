9fd6660ed63a186bb8395f87a4d3a359fbd4f5ae
Daniel Bizualem <danishow375@gmail.com>
2026-03-29 02:32:57 +0300

    backend setup

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,18 @@ import express from 'express';
 import cors from 'cors'
 import dotenv from 'dotenv'
 import morgan from 'morgan';
+import mongoose from 'mongoose';
+import { connectDB } from './src/config/db.js';
 
 
-
+dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 4000
 
 app.use(cors())
 app.use(express.json())
 
+connectDB()
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
